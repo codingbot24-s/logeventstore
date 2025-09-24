000da5e02ed98dc7a7aef9272164f453a99a6067
pcodefromarch <primecoder8@gmail.com>
2025-09-24 14:32:11 +0530

    fix now read working

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type LogFile struct {
 
 // create a new logfile return the * of struct
 func NewLogFile(fname string) (*LogFile, error) {
-	file, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	file, err := os.OpenFile(fname, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create log file: %w", err)
 	}
@@ -40,7 +40,7 @@ func NewTopic(name string, numPartitions int) (*Topic, error) {
 	}
 
 	partitions := make([]*LogFile, numPartitions)
-	for i := range numPartitions {
+	for i := 0; i < numPartitions; i++ {
 		logFile, err := NewLogFile(fmt.Sprintf("%s-partition-%d.log", name, i))
 		if err != nil {
 			return nil, fmt.Errorf("failed to create partition %d: %w", i, err)
@@ -69,14 +69,14 @@ func (t *Topic) readFromPartiton(key string, offset int) (string, error) {
 // close all partitions
 
 func (t *Topic) CloseP() error {
-	var err error
+	var Eerr error
 	for _, p := range t.partitions {
 		if err := p.Close(); err != nil {
-			err = err
+			Eerr = err
 		}
 	}
 
-	return err
+	return Eerr
 }
 
 // logfile write
@@ -122,16 +122,25 @@ func (l *LogFile) Close() error {
 	return nil
 }
 
+// TODO: expose the api for writing errror in reading
 func main() {
-	t, err := NewTopic("logs", 2)
+	t, err := NewTopic("logs", 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer t.CloseP()
 
-	err = t.writeIntoPartition("user-123", "user logged in")
+	key := "user-123"
+	err = t.writeIntoPartition(key, "user logged in\n")
 	if err != nil {
-		log.Printf("error writing in to the partition")
+		log.Printf("error writing to the partition: %v", err)
 	}
+
+	str, err := t.readFromPartiton(key, 2)
+	if err != nil {
+		log.Printf("error in reading: %v", err)
+	}
+
+	fmt.Println("Read message:", str)
 }
