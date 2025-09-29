5fd5fc5e610cfa04cfbd83654c99e76e87ea540b
pcodefromarch <primecoder8@gmail.com>
2025-09-29 13:10:12 +0530

    add helper

--- handler/handlers.go
+++ handler/handlers.go
@@ -2,128 +2,11 @@ package handlers
 
 import (
 	"fmt"
-	"hash/fnv"
-	"io"
-	"log"
 	"net/http"
-	"os"
 
+	"github.com/codingbot24-s/helper"
 	"github.com/gin-gonic/gin"
 )
-
-// represent 1partition
-type LogFile struct {
-	fileName string
-	file     *os.File
-	offset   int
-}
-
-// create a new logfile return the * of struct
-func NewLogFile(fname string) (*LogFile, error) {
-	file, err := os.OpenFile(fname, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create log file: %w", err)
-	}
-
-	return &LogFile{
-		fileName: fname,
-		file:     file,
-		offset:   0,
-	}, nil
-}
-
-type Topic struct {
-	partitions []*LogFile
-}
-
-// create a new topics with given partitions
-func NewTopic(name string, numPartitions int) (*Topic, error) {
-	if numPartitions <= 0 {
-		return nil, fmt.Errorf("number of partitions must be positive")
-	}
-
-	partitions := make([]*LogFile, numPartitions)
-	for i := 0; i < numPartitions; i++ {
-		logFile, err := NewLogFile(fmt.Sprintf("%s-partition-%d.log", name, i))
-		if err != nil {
-			return nil, fmt.Errorf("failed to create partition %d: %w", i, err)
-		}
-		partitions[i] = logFile
-	}
-
-	return &Topic{
-		partitions: partitions,
-	}, nil
-}
-
-// write into correct part
-func (t *Topic) writeIntoPartition(key string, message string) error {
-	part := t.get_partition(key)
-	return t.partitions[part].writeIntoLogFile(message)
-}
-
-// read from correct part
-
-func (t *Topic) readFromPartiton(key string, offset int) (string, error) {
-	part := t.get_partition(key)
-	return t.partitions[part].readFileFromOffset(offset)
-}
-
-// close all partitions
-
-func (t *Topic) CloseP() error {
-	var Eerr error
-	for _, p := range t.partitions {
-		if err := p.Close(); err != nil {
-			Eerr = err
-		}
-	}
-
-	return Eerr
-}
-
-// logfile write
-func (l *LogFile) writeIntoLogFile(str string) error {
-	if l.file == nil {
-		return fmt.Errorf("log file is not initialized")
-	}
-	n, err := l.file.Write([]byte(str))
-	if err != nil {
-		log.Fatal("error writing in logfile", l.fileName, err)
-	}
-	l.offset = l.offset + n
-	fmt.Printf("Writing successfull in %s\n", l.fileName)
-	return nil
-}
-
-// log file read
-func (l *LogFile) readFileFromOffset(offset int) (string, error) {
-	if l.file == nil {
-		return "", fmt.Errorf("log file is not initialized")
-	}
-	buf := make([]byte, 1024)
-	n, err := l.file.ReadAt(buf, int64(offset))
-	if err != nil && err != io.EOF {
-		log.Println("Error from reading in offset", err.Error())
-		return "", fmt.Errorf("failed to read from log file: %w", err)
-	}
-	return string(buf[:n]), nil
-}
-
-func (t *Topic) get_partition(key string) int {
-	hash := fnv.New32a()
-	hash.Write([]byte(key))
-	return int(hash.Sum32()) % len(t.partitions)
-}
-
-// close one log file
-func (l *LogFile) Close() error {
-	if l.file != nil {
-		return l.file.Close()
-	}
-	return nil
-}
-
 type produceReq struct {
 	TopicName string `json:"topicname" binding:"required"`
 	Key       string `json:"key" binding:"required"`
@@ -133,12 +16,11 @@ type produceReq struct {
 type consumeReq struct {
 	TopicName string `json:"topicname" binding:"required"`
 	Key       string `json:"key" binding:"required"`
-	Offset    int    `json:"offset" binding:"min=0"` 
+	Offset    int    `json:"offset" binding:"min=0"`
 }
-
 // we can create a topic map which will store all the topcis and we can use it to read from the topic
 // create a map to store all the topics
-var topicMap = make(map[string]*Topic)
+var topicMap = make(map[string]*helper.Topic)
 
 func Produce(c *gin.Context) {
 	var req produceReq
@@ -150,7 +32,7 @@ func Produce(c *gin.Context) {
 		return
 	}
 	// create a topic
-	topic, err := NewTopic(req.TopicName, 2)
+	topic,err := helper.NewTopic(req.TopicName, 2)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   "Failed to create topic",
@@ -160,8 +42,8 @@ func Produce(c *gin.Context) {
 	}
 	topicMap[req.TopicName] = topic
 
-	// Write message to partition
-	if err := topic.writeIntoPartition(req.Key, req.Message); err != nil {
+	// Write message to partition  
+	if err := topic.WriteIntoPartition(req.Key, req.Message); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   "Failed to write message",
 			"details": err.Error(),
@@ -198,7 +80,7 @@ func Consume(c *gin.Context) {
 		})
 		return
 	}
-	str, err := t.readFromPartiton(req.Key, req.Offset)
+	str, err := t.ReadFromPartiton(req.Key, req.Offset)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   "Failed to read from log file",
@@ -210,13 +92,12 @@ func Consume(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": str,
 		"details": "message has been read closing the files",
-		
 	})
 	//TODO: this will close the all the log file of all topic after reading from single topic this is the problem we need to solve this only close the one file not all file
 	for _, t := range topicMap {
-		err := t.CloseP()	
+		err := t.CloseP()
 		if err != nil {
 			fmt.Printf("Error closing topic: %v\n", err)
 		}
-	}	
+	}
 }
---
+++ helper/helpers.go
@@ -0,0 +1,123 @@
+package helper
+
+import (
+	"fmt"
+	"hash/fnv"
+	"io"
+	"log"
+	"os"
+)
+
+type LogFile struct {
+	fileName string
+	file     *os.File
+	offset   int
+}
+
+// create a new logfile return the * of struct
+func NewLogFile(fname string) (*LogFile, error) {
+	file, err := os.OpenFile(fname, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create log file: %w", err)
+	}
+
+	return &LogFile{
+		fileName: fname,
+		file:     file,
+		offset:   0,
+	}, nil
+}
+
+type Topic struct {
+	partitions []*LogFile
+}
+
+// create a new topics with given partitions
+func NewTopic(name string, numPartitions int) (*Topic, error) {
+	if numPartitions <= 0 {
+		return nil, fmt.Errorf("number of partitions must be positive")
+	}
+
+	partitions := make([]*LogFile, numPartitions)
+	for i := 0; i < numPartitions; i++ {
+		logFile, err := NewLogFile(fmt.Sprintf("%s-partition-%d.log", name, i))
+		if err != nil {
+			return nil, fmt.Errorf("failed to create partition %d: %w", i, err)
+		}
+		partitions[i] = logFile
+	}
+
+	return &Topic{
+		partitions: partitions,
+	}, nil
+}
+
+// write into correct part
+func (t *Topic) WriteIntoPartition(key string, message string) error {
+	part := t.Get_partition(key)
+	return t.partitions[part].WriteIntoLogFile(message)
+}
+
+// read from correct part
+
+func (t *Topic) ReadFromPartiton(key string, offset int) (string, error) {
+	part := t.Get_partition(key)
+	return t.partitions[part].ReadFileFromOffset(offset)
+}
+
+// close all partitions
+
+func (t *Topic) CloseP() error {
+	var Eerr error
+	for _, p := range t.partitions {
+		if err := p.Close(); err != nil {
+			Eerr = err
+		}
+	}
+
+	return Eerr
+}
+
+// logfile write
+func (l *LogFile) WriteIntoLogFile(str string) error {
+	if l.file == nil {
+		return fmt.Errorf("log file is not initialized")
+	}
+	n, err := l.file.Write([]byte(str))
+	if err != nil {
+		log.Fatal("error writing in logfile", l.fileName, err)
+	}
+	l.offset = l.offset + n
+	fmt.Printf("Writing successfull in %s\n", l.fileName)
+	return nil
+}
+
+// log file read
+func (l *LogFile) ReadFileFromOffset(offset int) (string, error) {
+	if l.file == nil {
+		return "", fmt.Errorf("log file is not initialized")
+	}
+	buf := make([]byte, 1024)
+	n, err := l.file.ReadAt(buf, int64(offset))
+	if err != nil && err != io.EOF {
+		log.Println("Error from reading in offset", err.Error())
+		return "", fmt.Errorf("failed to read from log file: %w", err)
+	}
+	return string(buf[:n]), nil
+}
+
+func (t *Topic) Get_partition(key string) int {
+	hash := fnv.New32a()
+	hash.Write([]byte(key))
+	return int(hash.Sum32()) % len(t.partitions)
+}
+
+// close one log file
+func (l *LogFile) Close() error {
+	if l.file != nil {
+		return l.file.Close()
+	}
+	return nil
+}
+
+
