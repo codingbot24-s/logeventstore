e72fceb98d59bf2181d6fa2caf90ed03b9dbdc9f
pcodefromarch <primecoder8@gmail.com>
2025-11-01 15:21:31 +0530

    correctly raeding the log broker file

---
+++ broker/cmd/broker2/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Broker struct {
+	NodeID int    `json:"nodeID"`
+	Port   int    `json:"port"`
+	Peers  []Peer `json:"peers"`
+}
+
+type Peer struct {
+	NodeID int `json:"nodeID"`
+	Port   int `json:"port"`
+}
+
+func main() {
+	filename := "broker2.json"
+
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		log.Printf("error opening file", err.Error())
+		return
+	}
+	data := make([]byte, 1024)
+	_, err = f.Read(data)
+	trimmed_data := bytes.Trim(data, "\x00")
+	if err != nil && err != io.EOF {
+		log.Printf("error reading file")
+		return
+	}
+	var b Broker
+	err = json.Unmarshal(trimmed_data, &b)
+	if err != nil {
+		fmt.Println("error unmarshalling", err.Error())
+	}
+
+	fmt.Println("Broker1 nodeID", b.NodeID)
+	fmt.Println("Broker1 port", b.Port)
+	for _, p := range b.Peers {
+		fmt.Println("Peer nodeID", p.NodeID)
+		fmt.Println("Peer port", p.Port)
+	}
+
+}
