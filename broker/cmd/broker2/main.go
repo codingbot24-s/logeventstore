ecc94225492080a1e012c5c2fc9a96eb4c1ef932
pfromarch <primecoder8@gmail.com>
2025-11-04 16:39:36 +0530

    add helpers in helpers

--- broker/cmd/broker2/main.go
+++ broker/cmd/broker2/main.go
@@ -1,111 +1,25 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"os"
 
+	"github.com/codingbot24-s/helper"
 	"github.com/gin-gonic/gin"
 )
 
-type Peer struct {
-	NodeID int `json:"nodeID"`
-	Port   int `json:"port"`
-}
-
-type Partition struct {
-	LeaderID int   `json:"leaderID"`
-	Replicas []int `json:"replicas"`
-}
-type Topic struct {
-	Partitions map[string]Partition
-}
-
-type Broker struct {
-	NodeID int    `json:"nodeID"`
-	Port   int    `json:"port"`
-	Peers  []Peer `json:"peers"`
-	Topics map[string]Topic
-}
-
-// map of topic and topic is map of partition
-// with this we can correclty read the cluster metadata
-type clusterMap map[string]map[string]Partition
-
-func read_cluster_metadata(filename string) (int, error) {
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR, 0644)
-	if err != nil {
-		return 0, fmt.Errorf("error opening file: %w", err)
-	}
-	defer f.Close()
-	data := make([]byte, 1024)
-	_, err = f.Read(data)
-	if err != nil && err != io.EOF {
-		return 0, fmt.Errorf("error reading file: %w", err)
-	}
-
-	trimmed_data := bytes.Trim(data, "\x00")
-
-	// Marshal the data into a clusterMap
-	var c clusterMap
-	err = json.Unmarshal(trimmed_data, &c)
-	if err != nil {
-		return 0, fmt.Errorf("error unmarshalling: %w", err)
-	}
-
-	// returing the clusterMap pointer
-
-	return c["test_topic"]["0"].LeaderID, nil
-}
-
-func readBrokerConfig(filename string) (int, error) {
-	// how to open a file from cmd dir
-
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR, 0644)
-	if err != nil {
-		return 0, fmt.Errorf("error opening file: %w", err)
-	}
-
-	data := make([]byte, 1023)
-	_, err = f.Read(data)
-	if err != nil && err != io.EOF {
-		return 0, fmt.Errorf("error reading file: %w", err)
-	}
-
-	trimmed_data := bytes.Trim(data, "\x00")
-
-	var b Broker
-	err = json.Unmarshal(trimmed_data, &b)
-
-	if err != nil {
-		return 0, fmt.Errorf("error unmarshaling json %w", err)
-	}
-
-	return b.NodeID, nil
-}
-
-func checkLeader(lId, nodeId int) bool {
-	if lId == nodeId {
-		return true
-	}
-	return false
-}
-
 func startReadConfig() {
 	clusterfilename := "../../cluster_meta.json"
-	brokerfilename := "broker1.json"
-	leaderID, err := read_cluster_metadata(clusterfilename)
+	brokerfilename := "broker2.json"
+	leaderID, err := helper.Read_cluster_metadata(clusterfilename)
 	if err != nil {
 		fmt.Println("error reading cluster metadata", err)
 	}
-	nodeId, err := readBrokerConfig(brokerfilename)
+	nodeId, err := helper.ReadBrokerConfig(brokerfilename)
 
 	if err != nil {
 		fmt.Println("error reading broker config %w", err)
 	}
-	if checkLeader(leaderID, nodeId) {
+	if helper.CheckLeader(leaderID, nodeId) {
 		fmt.Println("I am the leader of the test_topic")
 		return
 	}
@@ -120,7 +34,7 @@ func main() {
 	r := gin.Default()
 	r.POST("/produce", Produce)
 
-	fmt.Println("starting the server on 8081")
+	fmt.Println("starting the broker2 server on 8081")
 	r.Run(":8081")
 }
 
