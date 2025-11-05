8b5b76cb8b479586d123b6990d658aea38a36286
pfromarch <primecoder8@gmail.com>
2025-11-05 16:55:40 +0530

    add no we can get the broker

--- broker/cmd/broker1/main.go
+++ broker/cmd/broker1/main.go
@@ -3,34 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/codingbot24-s/helper"
 	"github.com/gin-gonic/gin"
 )
 
-func startReadConfig() {
-	clusterfilename := "../../cluster_meta.json"
-	brokerfilename := "broker1.json"
-	err := helper.ReadClusterMetadata(clusterfilename)
-	if err != nil {
-		fmt.Println("error reading cluster metadata", err)
-	}
-	err = helper.ReadConfig(brokerfilename)
 
-	if err != nil {
-		fmt.Println("error reading broker config %w", err)
-	}
-	// if helper.CheckLeader(leaderID, nodeId) {
-	// 	fmt.Println("I am the leader of the test_topic")
-	// 	return
-	// }
-	// fmt.Println("I am not the leader of the test_topic")
-}
 
 func main() {
-	go func() {
-		startReadConfig()
-	}()
-
 	r := gin.Default()
 	r.POST("/produce", Produce)
 
